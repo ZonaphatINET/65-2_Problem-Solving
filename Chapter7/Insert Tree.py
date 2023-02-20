54ada12500b8a34e5936296c89cc035e45bdd601
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-02-20 11:46:58 +0700

    update

---
+++ Chapter7/Insert Tree.py
@@ -0,0 +1,39 @@
+class Node:
+
+
+    def __init__(self, data):
+
+        self.left = Node
+        self.right = Node
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data< self.data:
+                if self.left is Node:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is Node:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+    
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data)
+        if self.right:
+            self.right.PrintTree()
+
+root = Node(10)
+root.insert(30)
+root.insert(40)
+root.insert(35)
+root.insert(20)
+root.insert(47)
+root.insert(5)
+root.PrintTree()
